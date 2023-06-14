8f00c691d4153e84eb65014e99f1e1d788931f28
hsujc <hsujc513@gmail.com>
2023-06-14 21:46:07 +0800

    delete main.py add test.py

---
+++ mongodb_for_search.py
@@ -0,0 +1,9 @@
+from full_text_search import FullTextSearch
+
+
+class MongoDBForSearch(FullTextSearch):
+    def __init__(self, mongodb_url, mongodb_username, mongodb_password):
+        pass
+
+    def search_keyword(self, keyword):
+        pass
